8423b3c2dc5a0a8f6126944dadfe5c683a12cc02
Dickson-Onyongo <dicksononyong@gmail.com>
2024-08-01 14:57:40 +0300

    pycode style corrections

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
+
 from flask import Flask
+
+
 """
 Script that starts flask web application on port 5000.
 
 """
+
+
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
+
     """
     Returns a greeting message.
 
     Returns
     -------
     str
-    	A greeting message "Hello HBNB!"
+        A greeting message "Hello HBNB!"
+
     """
+
     return "Hello HBNB!"
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
