e4f6ab1bfba11d8894bd736f2779037523e045be
Dickson-Onyongo <dicksononyong@gmail.com>
2024-08-21 12:58:15 +0300

    pycode styles update

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,48 +1,23 @@
 #!/usr/bin/python3
-"""
-
-Script that starts a Flask web application on host 0.0.0.0
+"""Script that starts a Flask web application on host 0.0.0.0
 and port 500.
-
 """
-
 
 from flask import Flask
-
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def hbnb():
+    """Returns a greeting message "Hello HBNB" """
     return "Hello HBNB"
-
-
-"""
-Returns a greeting message.
-
-Returns
-------
-str
-    Return: returns a greeting message, "Hello HBNB"
-
-"""
 
 
 @app.route("/hbnb", strict_slashes=False)
 def HBNB():
+    """Returns a short message "HBNB" """
     return"HBNB"
-
-
-"""
-Returns a short message.
-
-Returns
-------
-str
-    Return: returns a  message, "HBNB"
-
-"""
 
 
 if __name__ == '__main__':
